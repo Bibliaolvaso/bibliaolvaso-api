b1faad71aa57ff1d1bb35c6c5634435d004ac486
Laszlo Bacsi <lackac@lackac.hu>
2014-02-01 01:03:13 +0100

    Allow CORS from xip.io and CloudFront host

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,8 @@
 Rails.application.config.middleware.use Rack::Cors do
   allow do
-    origins 'localhost:8181', 'bibliaolvaso.dev', 'bibliaolvaso.hu'
+    origins 'bibliaolvaso.hu', 'd16lfcr0o0mhas.cloudfront.net',
+            'localhost:8181', 'bibliaolvaso.dev', 'bibliaolvaso.hu',
+            /http:\/\/bibliaolvaso(?:\.\d+){4}\.xip\.io/
     resource '*', methods: :any, max_age: 3600
   end
 end
